2db40eb525cdf987eac64728affb3d532fdf7cfd
Robin de Rooij <rderooij685@gmail.com>
2023-04-26 16:21:39 +0200

    Add publish.js

---
+++ publish.js
@@ -0,0 +1,68 @@
+const fs = require('fs');
+
+function parsePackage() {
+  const data = fs.readFileSync('./package.json',
+                               {encoding:'utf8', flag:'r'});
+  return JSON.parse(data)
+}
+
+/**
+ * Publish hook for the JSDoc template.  Writes to JSON stdout.
+ * @param {function} data The root of the Taffy DB containing doclet records.
+ * @param {Object} opts Options.
+ */
+exports.publish = function(data, opts) {
+  const docs = data().get().filter(function(doc) {
+    return !doc.undocumented;
+  });
+  const classes = data({kind: "class"}).get()
+  const functions = data({kind: "function"}).get().filter(x => !x.undocumented)
+  const outputClasses = classes.map(class_ => {
+    const classFunctions = functions.filter(x => x.memberof === class_.name)
+    const functionOutput = classFunctions.map(func => {
+      let params = []
+      if (typeof func.params !== 'undefined') {
+        params = func.params.map(param => {
+          return {
+            '@type': 'Parameter',
+            name: param.name,
+            type: param.type ? param.type.names.join("|") : 'unknown',
+            summary: param.description
+          }
+      })
+      }
+      return {
+        '@type': 'Definition',
+        name: func.name,
+        summary: func.description,
+        parameters: params,
+        returns: (typeof func.returns !== 'undefined' ? func.returns.map(returns => {
+          let type = "void"
+          if (typeof returns.type !== 'undefined') {
+            type = returns.type.names.filter(x => x != null).join("|")
+          }
+          return {'@type': 'Returns', name: '', summary: returns.description, type }
+        })[0] : {'@type': 'Returns', name: '', type: 'void' })
+      }
+    })
+    return {
+      '@type': 'Class',
+      name: class_.name,
+      summary: class_.description,
+      memberFunctions: functionOutput,
+    }
+  })
+  const package = parsePackage()
+  const application = {
+    '@type': 'Application',
+    language: "Javascript",
+    version: package['version'],
+    license: package['license'],
+    name: package['name'],
+    summary: package['description'],
+    modules: [
+      {'@type': 'Module', name: "lib", classes: outputClasses}
+    ],
+  }
+  console.log(JSON.stringify(application, null, 2))
+};
